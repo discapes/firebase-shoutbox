76016fa432ba847bc7478c0c1ccfdb457323bb59
Miika Tuominen <miika.km.tuominen@gmail.com>
2023-02-14 12:32:18 +0200

    compiles

--- src/lib/firebase.server.ts
+++ src/lib/firebase.server.ts
@@ -1,19 +1,21 @@
-import { credential } from 'firebase-admin';
-import { initializeApp } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
-import { getAuth } from 'firebase-admin/auth';
+import { initializeApp, cert } from 'firebase-admin/app';
+import { Firestore, getFirestore } from 'firebase-admin/firestore';
+import { Auth, getAuth } from 'firebase-admin/auth';
 import { SERVICE_ACCOUNT_EMAIL, SERVICE_ACCOUNT_KEY } from '$env/static/private';
+import { building } from '$app/environment';
 
-initializeApp(
-	{
-		credential: credential.cert({
-			projectId: 'so-me-22719',
-			clientEmail: SERVICE_ACCOUNT_EMAIL,
-			privateKey: SERVICE_ACCOUNT_KEY
-		})
-	},
-	'so-me'
-);
+if (!building) {
+	initializeApp(
+		{
+			credential: cert({
+				projectId: 'so-me-22719',
+				clientEmail: SERVICE_ACCOUNT_EMAIL,
+				privateKey: SERVICE_ACCOUNT_KEY
+			})
+		},
+		'so-me'
+	);
+}
 
-export const db = getFirestore();
-export const auth = getAuth();
+export const db: Firestore = <Firestore>(!building && getFirestore());
+export const auth: Auth = <Auth>(!building && getAuth());
